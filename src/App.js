5d78211ed1a0c273bf65a9300fb335109b66fe17
kreison <alankainazovr@gmail.com>
2022-06-30 02:00:49 +0600

    reinstall

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
-      main page
+      <MainPage/>
+      
     </div>
   );
 }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+const MainPage = () => {
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+};
+
+export default MainPage;
